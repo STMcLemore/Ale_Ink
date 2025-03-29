d2d814458dc0dd321ccd9c107f744053617cfbfd
Stephen McLemore <stmclemore@hotmail.com>
2025-03-28 21:31:46 -0400

    Added Services folder and NoteService class to API project.

---
+++ Ale_Ink.API/Services/NoteService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ale_Ink.Models;
+using Ale_Ink.Repositories;
+
+namespace Ale_Ink.API.Services
+{
+    public class NoteService
+    {
+        private readonly IGenericRepository<Note> _noteRepository;
+
+        public NoteService(IGenericRepository<Note> noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+
+        public async Task<IEnumerable<Note>> GetAllNotes()
+        {
+            return await _noteRepository.GetAllAsync();
+        }
+
+        public async Task<Note> GetNoteById(int id)
+        {
+            return await _noteRepository.GetByIdAsync(id);
+        }
+
+        public async Task AddNoteAsync(Note note)
+        {
+            await _noteRepository.AddAsync(note);
+        }
+
+        public async Task UpdateNoteAsync(Note note)
+        {
+            await _noteRepository.UpdateAsync(note);
+        }
+
+        public async Task DeleteNoteAsync(int id)
+        {
+            var note = await _noteRepository.GetByIdAsync(id);
+            if (note != null)
+            {
+                await _noteRepository.DeleteAsync(note);
+            }
+        }
+    }
+}
